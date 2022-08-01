c938fc2741e10012c68070f71e3ddb7b2359a724
hakuna <943383098@qq.com>
2022-08-01 17:59:25 +0800

    fix send-lark-message remark undefined

--- scripts/sendMessageToLark.js
+++ scripts/sendMessageToLark.js
@@ -114,7 +114,7 @@ const larkMessageJSON = (messageConfigObject.message && messageConfigObject.lark
             {
                 "elements": [
                     {
-                        "content": `${messageConfigObject.deployToolsText || `${messageConfigObject.deployTools ? `Deploy through ${messageConfigObject.deployTools}` : ''}`} ${(messageConfigObject.deployTools || messageConfigObject.deployToolsText) && messageConfigObject.remark ? '\n' : ''} ${messageConfigObject.remark}`,
+                        "content": `${messageConfigObject.deployToolsText || `${messageConfigObject.deployTools ? `Deploy through ${messageConfigObject.deployTools}` : ''}`} ${(messageConfigObject.deployTools || messageConfigObject.deployToolsText) && messageConfigObject.remark ? '\n' : ''}${messageConfigObject.remark || ''}`,
                         "tag": "plain_text"
                     }
                 ],
